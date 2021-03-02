ef4041eb93e7e1b184d95bee7ecc1565797db466
Fled8617 <35049517+Fled8617@users.noreply.github.com>
2021-03-02 21:28:27 +0800

    更新为hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 import { parse } from "querystring";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Antd from "./example/antd";
 import LeetCode from "./example/leetcode";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/">
             <Antd />
@@ -16,7 +16,7 @@ function App() {
             <LeetCode />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
